11a376435b262f5044174bc6b523435e864c6799
17070610 <schimea@hotmail.com>
2025-06-04 11:01:43 +0200

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""The authentication module"""
+import bcrypt
+
+def _hash_password(password: str)-> bytes:
+    """Turns a normal password into a hashed password"""
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
